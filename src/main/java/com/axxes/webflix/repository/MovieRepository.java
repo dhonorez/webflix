394b93e67f79b702dd6febf8c9906027573f7e49
Dylan Honorez <dylan.honorez@axxes.com>
2019-03-04 17:44:41 +0100

    Use this as working branch

--- src/main/java/com/axxes/webflix/repository/MovieRepository.java
+++ src/main/java/com/axxes/webflix/repository/MovieRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface MovieRepository extends JpaRepository<Movie, Integer> {
+public interface MovieRepository {
 
     Movie findById(int id);
 
